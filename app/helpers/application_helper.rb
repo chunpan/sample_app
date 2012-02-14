0f2299c7ca63404896cbd88abe39c045a4707dc7
Billy Pan <bpan@reverbnation.com>
2012-02-13 20:12:54 -0500

    Finished exercises after Chapter 5

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,11 @@
 module ApplicationHelper
 
+  # Return a logo image tag.
+  def logo
+    image_tag("logo.png", :alt => "Sample App", :class => "round")
+  end
+  
+  # Return a title on a per-page basis.
   def title
     base_title = "Ruby on Rails Tutorial Sample App"
     if @title.nil?
