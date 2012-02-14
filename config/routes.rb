87166a05f0b3ce647b7089231c4dbd51559ff1ff
Billy Pan <bpan@reverbnation.com>
2012-02-13 20:05:44 -0500

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 SampleApp::Application.routes.draw do
-  get "pages/home"
+  get "users/new"
 
-  get "pages/contact"
+  match '/contact', :to => 'pages#contact'
+  match '/about', :to => 'pages#about'
+  match '/help', :to => 'pages#help'
+  match '/signup', :to => 'users#new'
   
-  get "pages/about"
+  root :to => 'pages#home'
   
-  get "pages/help"
-
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
